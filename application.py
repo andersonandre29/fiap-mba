0e965f5d6a3d49f4437c96db0e52c02313878aa6
F0733420 Anderson Andre dos Santos <andersonandre@bb.com.br>
2022-09-11 20:15:04 -0300

    Entrega trabalho

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "<h1>Trabalho FIAP! GITHUB </h1>\nMBA! o/"
+    return "<h1>Trabalho Fiap - 1MOBRBB</h1><h3>CLOUD ARCHITECTURE & DEVOPS</h2><h3>Alunos</h3><h4>Anderson André dos Santos</h4><h4>Adriano Bastos e Silva Moraes</h4><h4>Gustavo Souza Rodrigues</h4><h4>Luis Fernando de Sena Carita</h4><h4>Paulo Roberto Petrilho</h4>\nMBA! o/"
 
 if __name__ == '__main__':
     # application.run()
